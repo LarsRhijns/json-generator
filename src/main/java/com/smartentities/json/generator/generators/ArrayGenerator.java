c0cb08a06c4fe6d4d3095f8390db6255bbe3277f
Lars <l.e.rhijnsburger@student.tudelft.nl>
2021-05-18 16:18:45 +0200

    Add Array generator

--- src/main/java/com/smartentities/json/generator/generators/ArrayGenerator.java
+++ src/main/java/com/smartentities/json/generator/generators/ArrayGenerator.java
@@ -6,27 +6,71 @@ import org.json.JSONArray;
 
 import com.smartentities.json.generator.GeneratorFactory;
 
+import java.util.List;
+import java.util.Random;
+
 public class ArrayGenerator extends JsonValueGenerator<JSONArray> {
 
 	public ArrayGenerator(Schema schema) {
 		super(schema);
 	}
 
+	/**
+	 * Generates the items specified in the array. Assumes that the amount of items is within the range specified
+	 * in the schema.
+	 * @return JSONArray output: A valid array as specified in the schema.
+	 */
 	@Override
-	// TODO Parse properties of the array
 	public JSONArray generate() {
-
 		if (schema instanceof ArraySchema) {
-			ArraySchema arraySchema = (ArraySchema) schema;
+			Random random = new Random();
+			ArraySchema sc = (ArraySchema) schema;
+			List<Schema> schemas = sc.getItemSchemas();
+			int lenItems = schemas.size();
+			Schema allItemSchema = sc.getAllItemSchema();
+			JSONArray output = new JSONArray();
+			int minItems = 0;
+			boolean hasMinItems = true;
+			int maxItems = 0;
+			boolean hasMaxItems = true;
 
-			Schema allItemSchema = arraySchema.getAllItemSchema();
+			try {
+				minItems = sc.getMinItems();
+			} catch (NullPointerException e) {
+				hasMinItems = false;
+			}
 
-			JSONArray jsonArray = new JSONArray();
+			try {
+				maxItems = sc.getMaxItems();
+			} catch (NullPointerException e) {
+				hasMaxItems = false;
+			}
 
-			jsonArray.put(GeneratorFactory.getGenerator(allItemSchema).generate());
+			// Check if correct amount of items are specified
+			if (hasMaxItems && hasMinItems) {
+				if (maxItems < minItems) {
+					throw new IllegalArgumentException("Minimal length should be smaller than maximal length. Please check schema.");
+				}
 
-			return jsonArray;
+				if (lenItems > maxItems || lenItems < minItems) {
+					throw new IllegalArgumentException("Too many/few items specified. Please check schema.");
+				}
+			} else if (hasMaxItems) {
+				if (lenItems > maxItems) {
+					throw new IllegalArgumentException("Too many items specified. Please check schema.");
+				}
+			} else if (hasMinItems) {
+				if (lenItems < minItems) {
+					throw new IllegalArgumentException("Too few items specified. Please check schema.");
+				}
+			}
+
+			for (Schema s : schemas) {
+				output.put(GeneratorFactory.getGenerator(s).generate());
+			}
+
+			return output;
 		}
-		return new JSONArray();
+		return null;
 	}
 }
